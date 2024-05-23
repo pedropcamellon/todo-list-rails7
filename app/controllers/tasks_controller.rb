e91ed237d5dc7b5fecfee93972d6b6b28ab7ae86
CodeSandbox <support@codesandbox.io>
2024-05-23 17:54:56 +0000

    Sandbox snapshot commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,16 @@
 class TasksController < ApplicationController
-  before_action :set_project
-  validates :project, presence: true
+  before_action :set_project, only: %i[ create ]
+  before_action :set_task, only: %i[ destroy ]
 
   # POST /tasks or /tasks.json
   def create
     @task = @project.tasks.create(task_params)
+    redirect_to @project
+  end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy
     redirect_to @project
   end
 
@@ -13,6 +19,10 @@ class TasksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_project
         @project = Project.find(params[:project_id])
+    end
+
+    def set_task
+        @task = @project.tasks.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
